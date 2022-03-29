cd210e6142e016c7e7fc6d1c01d1e63a26b6a294
Your Name <you@example.com>
2022-03-29 15:16:32 +0200

    Połączenie z API - wer. Beta

--- lab02ConsoleApp/ConsoleApp1/Program.cs
+++ lab02ConsoleApp/ConsoleApp1/Program.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.IO;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            var json = File.ReadAllText("data.json");
+            string call = "https://openexchangerates.org/api/historical/2022-03-28.json?app_id=8d4a6a6c55ad4d93896b50921077c1d9&symbols=PLN,GBP,EUR,CHF";
+            HttpClient client = new HttpClient();
+            string response = await client.GetStringAsync(call);
+            Console.WriteLine(response);
 
-            Console.WriteLine(json);
+     
 
         }
     }
