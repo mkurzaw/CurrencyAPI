0aef39df0345026c188d5f743f859f0bf3d36e7b
Your Name <you@example.com>
2022-03-29 13:52:15 +0200

    Szkielet_Konsoli

---
+++ lab02ConsoleApp/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var json = File.ReadAllText("data.json");
+
+            Console.WriteLine(json);
+
+        }
+    }
+}
