296789a1f4682493e415cf2e3400a48215c032a9
Your Name <you@example.com>
2022-03-29 16:54:51 +0200

    Połączenie z API + parametr

--- lab02ConsoleApp/ConsoleApp1/Program.cs
+++ lab02ConsoleApp/ConsoleApp1/Program.cs
@@ -11,8 +11,27 @@ namespace ConsoleApp1
     {
         static async Task Main(string[] args)
         {
+            Console.WriteLine("Podaj datę w formacie: RRRR-MM-DD\t");
+            string date;
+            date = Console.ReadLine();
+
+            /*
+            // obsługa błędu niepoprawnego formatu
+            string yearString = date[0] + date[1] + date[2] + date[3];
+            int tmp0 = Int32.Parse(yearString);
+            int tmp1 = Convert.ToInt32(date[1]);
+            int tmp2 = Convert.ToInt32(date[2]);
+            int tmp3 = Convert.ToInt32(date[3]);
+            int year = (tmp0*1000) + (tmp1*100) + (tmp2*10) + (tmp3);
+            Console.WriteLine(tmp0);
+            Console.WriteLine(tmp1);
+            Console.WriteLine(year);
+            */
+
+           
+
             // wysłanie rządania do serwisu API oraz zapis w zmiennej
-            string call = "https://openexchangerates.org/api/historical/2022-03-28.json?app_id=8d4a6a6c55ad4d93896b50921077c1d9&symbols=PLN,GBP,EUR,CHF";
+            string call = $"https://openexchangerates.org/api/historical/{date}.json?app_id=8d4a6a6c55ad4d93896b50921077c1d9&symbols=PLN,GBP,EUR,CHF";
             HttpClient client = new HttpClient();
             string response = await client.GetStringAsync(call);
 
@@ -23,7 +42,7 @@ namespace ConsoleApp1
             var json = File.ReadAllText("data.json");
             Console.WriteLine(json);
 
-
+            
      
 
         }
